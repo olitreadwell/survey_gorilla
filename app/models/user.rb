943d0bd9bc074ae72641f6957dff19e7b3615889
name": null, & Andrew Larson <pair+olitreadwell.omgmakeme@devbootcamp.com>
2014-05-23 11:26:46 -0500

    Authentication & BCrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
+  include BCrypt
+
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
 
@@ -9,4 +13,23 @@ class User < ActiveRecord::Base
            foreign_key: "creator_id" # A user has many surveys they have created
 
   has_many :user_answers # A user has many questions that they have responded to
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
+  def self.authenticate(email, password)
+    user = User.find_by_email(email)
+    if user && user.password == password
+      user
+    else
+      nil
+    end
+  end
+
 end

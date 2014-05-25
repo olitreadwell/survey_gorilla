a80cce00754a8acf647278610b52dd9066994a92
Ariel K Diamond & Beth <pair+arieldiamond.cynaria@devbootcamp.com>
2014-05-25 16:18:07 -0500

    Beth is awesome and completed AJAX -Andrew

--- public/js/application.js
+++ public/js/application.js
@@ -1,22 +1,23 @@
 $(document).ready(function() {
-  var Survey = function(user, title, id, questions) {
-    this.user = user;
-    this.title = title;
-    this.id = id;
-    this.questions = questions || [];
-  }
-
-  Survey.prototype.getQuestionForm = function(){
-    $.get("/survey/"+this.id+"/question/new", function(getResponse){
-        $("#create_survey").append(getResponse);
-      });
+  var Survey = {
+    init: function(user, title, id, questions) {
+                   this.user = user;
+                   this.title = title;
+                   this.id = id;
+                   this.questions = questions || [];
+                 },
+    getQuestionForm: function(){
+      $.get("/survey/"+this.id+"/question/new", function(getResponse){
+          $("#create_survey").append(getResponse);
+        });
+      },
+    addQuestion: function(question){
+      this.questions.push(question);
+    }
   };
 
-  Survey.prototype.add_question = function(question){
-    this.questions.push(question);
-  }
 
-  var Question = function(surveyId, questionText, id) {
+  function Question (surveyId, questionText, id) {
     this.surveyId = surveyId;
     this.questionText = questionText;
     this.id = id;
@@ -30,28 +31,27 @@ $(document).ready(function() {
     var data = $("#new_survey").serialize();
 
     $.post(url, data, function(response){
-      survey = new Survey(response.survey.creator_id,
-                              response.survey.title,
-                              response.survey.id);
+      Survey.init(response.survey.creator_id,
+                  response.survey.title,
+                  response.survey.id);
       $("#new_survey").remove();
-      $("#create_survey").append("<h2>" + survey.title + "</h2>")
-      survey.getQuestionForm();
+      $("#create_survey").append("<h2>" + Survey.title + "</h2>")
+      Survey.getQuestionForm();
     },"json");
   });
 
-  $("#create_survey").on("submit", "#new_question", function(e){
+  $("#create_survey").on("submit", "#new_question_form", function(e){
     e.preventDefault();
-    console.log("this happens");
-    var newQuestionUrl = $("#new_question").attr("action");
-    var newQuestionData = $("#new_question").serialize();
-    $("#new_question").remove();
-    console.log(newQuestionData);
+    var newQuestionUrl = $("#new_question_form").attr("action");
+    var newQuestionData = $("#new_question_form").serialize();
+    $("#new_question_form").remove();
     $.post(newQuestionUrl, newQuestionData, function(response){
+      $("#new_question").remove();
       var question = new Question(response.question.survey_id,
                                   response.question.question_text,
                                   response.question.id);
-      survey.add_question(question);
-      survey.getQuestionForm();
+      Survey.addQuestion(question);
+      Survey.getQuestionForm();
     },"json");
   });
 });
